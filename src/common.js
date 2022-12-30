569b82701914c5da19bf6c6fae7358a2315cda85
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2022-12-30 13:46:16 +0300

    fix(regex): restrict expression for diacritics only

--- src/common.js
+++ src/common.js
@@ -67,7 +67,7 @@ export const parseEpub = (htmlDocs, mwbYear, lang) => {
 		const wdHtml = htmlItem.getElementsByTagName('h1').item(0);
 		const weekDate = wdHtml.textContent.replaceAll(/\u00A0/g, ' ');
 
-		const dayParse = weekDate.match(/((\w|\s)*\w(?=")|\w|[A-zÀ-ž])+/g);
+		const dayParse = weekDate.match(/((\w|\s)*\w(?=")|\w|[À-ž])+/g);
 		let varDay;
 		let varMonthName;
 
