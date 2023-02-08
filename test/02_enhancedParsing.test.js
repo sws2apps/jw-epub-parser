2a1e3d9f3534f2e5a474f8e898e086f15eca717b
rhahao <26148770+rhahao@users.noreply.github.com>
2023-02-08 07:31:02 +0300

    skip live testing

--- test/02_enhancedParsing.test.js
+++ test/02_enhancedParsing.test.js
@@ -11,100 +11,57 @@ const JW_FINDER = 'https://www.jw.org/finder?';
 
 describe('Triggered enhanced parsing', () => {
 	for (let i = 0; i < list.length; i++) {
-		if (!list.skipLive) {
-			const { language, issue } = list[i];
+		const { language, issue } = list[i];
 
-			const fetchData = new Promise(async (resolve, reject) => {
-				try {
-					let fixture = [];
-					let data = [];
+		const fetchData = new Promise(async (resolve, reject) => {
+			try {
+				let fixture = [];
+				let data = [];
 
-					const url =
-						JW_CDN +
-						new URLSearchParams({
-							langwritten: language,
-							pub: 'mwb',
-							output: 'json',
-							issue,
-						});
+				const url =
+					JW_CDN +
+					new URLSearchParams({
+						langwritten: language,
+						pub: 'mwb',
+						output: 'json',
+						issue,
+					});
 
-					const res = await fetch(url);
+				const res = await fetch(url);
 
-					if (res.status === 200) {
-						const result = await res.json();
-						const hasEPUB = result.files[language].EPUB;
+				if (res.status === 200) {
+					const result = await res.json();
+					const hasEPUB = result.files[language].EPUB;
 
-						if (hasEPUB) {
-							const epubFile = hasEPUB[0].file;
-							const epubUrl = epubFile.url;
-							const epubName = path.basename(epubUrl);
-							data = await loadEPUB({ url: epubUrl });
-							fixture = (await import(`./fixtures/${epubName.replace('.epub', '.js')}`)).default;
-						}
-
-						if (!hasEPUB) {
-							const url =
-								JW_FINDER +
-								new URLSearchParams({
-									wtlocale: language,
-									pub: 'mwb',
-									issue,
-								});
-
-							const res = await fetch(url);
-							const result = await res.text();
-
-							const parser = new window.DOMParser();
-							const htmlItem = parser.parseFromString(result, 'text/html');
-
-							const docIds = [];
-							const accordionItems = htmlItem.getElementsByClassName('docClass-106');
-							for (const weekLink of accordionItems) {
-								weekLink.classList.forEach((item) => {
-									if (item.indexOf('docId-') !== -1) {
-										docIds.push(item.split('-')[1]);
-									}
-								});
-							}
-
-							const htmlDocs = [];
-							for (let z = 0; z < docIds.length; z++) {
-								const docId = docIds[z];
-								const finderLink = `https://www.jw.org/finder?wtlocale=${language}&docid=${docId}`;
-								const res2 = await fetch(finderLink);
-								const result2 = await res2.text();
-
-								const parser = new window.DOMParser();
-								const htmlItem = parser.parseFromString(result2, 'text/html');
-								htmlDocs.push(htmlItem);
-							}
-
-							data = await loadEPUB({ htmlDocs, mwbYear: issue.substring(0, 4), lang: language });
-							fixture = (await import(`./fixtures/mwb_${language}_${issue}.js`)).default;
-						}
+					if (hasEPUB) {
+						const epubFile = hasEPUB[0].file;
+						const epubUrl = epubFile.url;
+						const epubName = path.basename(epubUrl);
+						data = await loadEPUB({ url: epubUrl });
+						fixture = (await import(`./fixtures/${epubName.replace('.epub', '.js')}`)).default;
 					}
-
-					resolve({ data, fixture });
-				} catch (err) {
-					reject(err);
 				}
-			});
 
-			it(`Test loadEPUB function with enhanced parsing for ${language} language`, (done) => {
-				fetchData.then((result) => {
-					const { data, fixture } = result;
+				resolve({ data, fixture });
+			} catch (err) {
+				reject(err);
+			}
+		});
 
-					expect(data.mwbYear).to.equal(fixture.mwbYear);
-					expect(data.weeksCount).to.equal(fixture.weeksCount);
-					for (let a = 0; a < fixture.weeksData.length; a++) {
-						const week = fixture.weeksData[a];
-						for (let [key, value] of Object.entries(week)) {
-							expect(data.weeksData[a]).to.have.property(key).equal(value);
-						}
+		it(`Test loadEPUB function with enhanced parsing for ${language} language`, (done) => {
+			fetchData.then((result) => {
+				const { data, fixture } = result;
+
+				expect(data.mwbYear).to.equal(fixture.mwbYear);
+				expect(data.weeksCount).to.equal(fixture.weeksCount);
+				for (let a = 0; a < fixture.weeksData.length; a++) {
+					const week = fixture.weeksData[a];
+					for (let [key, value] of Object.entries(week)) {
+						expect(data.weeksData[a]).to.have.property(key).equal(value);
 					}
-					done();
-				});
+				}
+				done();
 			});
-		}
+		});
 	}
 });
