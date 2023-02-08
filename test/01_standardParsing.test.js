73e9f7254d0aedbf2b759b771c0524e7c048f414
rhahao <26148770+rhahao@users.noreply.github.com>
2023-02-09 00:09:38 +0300

    update test

--- test/01_standardParsing.test.js
+++ test/01_standardParsing.test.js
@@ -16,6 +16,7 @@ const fetchData = async (language, issue) => {
 		new URLSearchParams({
 			langwritten: language,
 			pub: 'mwb',
+			fileformat: 'epub',
 			output: 'json',
 			issue,
 		});
@@ -24,56 +25,13 @@ const fetchData = async (language, issue) => {
 
 	if (res.status === 200) {
 		const result = await res.json();
-		const hasEPUB = result.files[language].EPUB;
+		const epubEntry = result.files[language].EPUB;
 
-		if (hasEPUB) {
-			const epubFile = hasEPUB[0].file;
-			const epubUrl = epubFile.url;
-			const epubName = path.basename(epubUrl);
-			data = await loadEPUB({ url: epubUrl });
-			fixture = (await import(`./fixtures/${epubName.replace('.epub', '.js')}`)).default;
-		}
-
-		if (!hasEPUB) {
-			const url =
-				JW_FINDER +
-				new URLSearchParams({
-					wtlocale: language,
-					pub: 'mwb',
-					issue,
-				});
-
-			const res = await fetch(url);
-			const result = await res.text();
-
-			const parser = new window.DOMParser();
-			const htmlItem = parser.parseFromString(result, 'text/html');
-
-			const docIds = [];
-			const accordionItems = htmlItem.getElementsByClassName('docClass-106');
-			for (const weekLink of accordionItems) {
-				weekLink.classList.forEach((item) => {
-					if (item.indexOf('docId-') !== -1) {
-						docIds.push(item.split('-')[1]);
-					}
-				});
-			}
-
-			const htmlDocs = [];
-			for (let z = 0; z < docIds.length; z++) {
-				const docId = docIds[z];
-				const finderLink = `https://www.jw.org/finder?wtlocale=${language}&docid=${docId}`;
-				const res2 = await fetch(finderLink);
-				const result2 = await res2.text();
-
-				const parser = new window.DOMParser();
-				const htmlItem = parser.parseFromString(result2, 'text/html');
-				htmlDocs.push(htmlItem);
-			}
-
-			data = await loadEPUB({ htmlDocs, mwbYear: issue.substring(0, 4), lang: language });
-			fixture = (await import(`./fixtures/mwb_${language}_${issue}.js`)).default;
-		}
+		const epubFile = epubEntry[0].file;
+		const epubUrl = epubFile.url;
+		const epubName = path.basename(epubUrl);
+		data = await loadEPUB({ url: epubUrl });
+		fixture = (await import(`./fixtures/${epubName.replace('.epub', '.js')}`)).default;
 	}
 
 	return { data, fixture };
--- test/02_enhancedParsing.test.js
+++ test/02_enhancedParsing.test.js
@@ -7,7 +7,6 @@ import { loadEPUB } from '../dist/node/index.js';
 const list = JSON.parse(await fs.promises.readFile(new URL('./enhancedParsing/list.json', import.meta.url)));
 
 const JW_CDN = 'https://app.jw-cdn.org/apis/pub-media/GETPUBMEDIALINKS?';
-const JW_FINDER = 'https://www.jw.org/finder?';
 
 const fetchData = async (language, issue) => {
 	let data = [];
@@ -18,6 +17,7 @@ const fetchData = async (language, issue) => {
 		new URLSearchParams({
 			langwritten: language,
 			pub: 'mwb',
+			fileformat: 'epub',
 			output: 'json',
 			issue,
 		});
@@ -26,56 +26,13 @@ const fetchData = async (language, issue) => {
 
 	if (res.status === 200) {
 		const result = await res.json();
-		const hasEPUB = result.files[language].EPUB;
+		const epubEntry = result.files[language].EPUB;
 
-		if (hasEPUB) {
-			const epubFile = hasEPUB[0].file;
-			const epubUrl = epubFile.url;
-			const epubName = path.basename(epubUrl);
-			data = await loadEPUB({ url: epubUrl });
-			fixture = (await import(`./fixtures/${epubName.replace('.epub', '.js')}`)).default;
-		}
-
-		if (!hasEPUB) {
-			const url =
-				JW_FINDER +
-				new URLSearchParams({
-					wtlocale: language,
-					pub: 'mwb',
-					issue,
-				});
-
-			const res = await fetch(url);
-			const result = await res.text();
-
-			const parser = new window.DOMParser();
-			const htmlItem = parser.parseFromString(result, 'text/html');
-
-			const docIds = [];
-			const accordionItems = htmlItem.getElementsByClassName('docClass-106');
-			for (const weekLink of accordionItems) {
-				weekLink.classList.forEach((item) => {
-					if (item.indexOf('docId-') !== -1) {
-						docIds.push(item.split('-')[1]);
-					}
-				});
-			}
-
-			const htmlDocs = [];
-			for (let z = 0; z < docIds.length; z++) {
-				const docId = docIds[z];
-				const finderLink = `https://www.jw.org/finder?wtlocale=${language}&docid=${docId}`;
-				const res2 = await fetch(finderLink);
-				const result2 = await res2.text();
-
-				const parser = new window.DOMParser();
-				const htmlItem = parser.parseFromString(result2, 'text/html');
-				htmlDocs.push(htmlItem);
-			}
-
-			data = await loadEPUB({ htmlDocs, mwbYear: issue.substring(0, 4), lang: language });
-			fixture = (await import(`./fixtures/mwb_${language}_${issue}.js`)).default;
-		}
+		const epubFile = epubEntry[0].file;
+		const epubUrl = epubFile.url;
+		const epubName = path.basename(epubUrl);
+		data = await loadEPUB({ url: epubUrl });
+		fixture = (await import(`./fixtures/${epubName.replace('.epub', '.js')}`)).default;
 	}
 
 	return { data, fixture };
