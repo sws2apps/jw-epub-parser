ff6da293cd026b01c3272c860eb51c81a8f70462
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2023-02-08 08:12:45 +0300

    chore(module): skip live testing for some languages

--- test/02_enhancedParsing.test.js
+++ test/02_enhancedParsing.test.js
@@ -40,47 +40,6 @@ describe('Triggered enhanced parsing', () => {
 						data = await loadEPUB({ url: epubUrl });
 						fixture = (await import(`./fixtures/${epubName.replace('.epub', '.js')}`)).default;
 					}
-
-					if (!hasEPUB) {
-						const url =
-							JW_FINDER +
-							new URLSearchParams({
-								wtlocale: language,
-								pub: 'mwb',
-								issue,
-							});
-
-						const res = await fetch(url);
-						const result = await res.text();
-
-						const parser = new window.DOMParser();
-						const htmlItem = parser.parseFromString(result, 'text/html');
-
-						const docIds = [];
-						const accordionItems = htmlItem.getElementsByClassName('docClass-106');
-						for (const weekLink of accordionItems) {
-							weekLink.classList.forEach((item) => {
-								if (item.indexOf('docId-') !== -1) {
-									docIds.push(item.split('-')[1]);
-								}
-							});
-						}
-
-						const htmlDocs = [];
-						for (let z = 0; z < docIds.length; z++) {
-							const docId = docIds[z];
-							const finderLink = `https://www.jw.org/finder?wtlocale=${language}&docid=${docId}`;
-							const res2 = await fetch(finderLink);
-							const result2 = await res2.text();
-
-							const parser = new window.DOMParser();
-							const htmlItem = parser.parseFromString(result2, 'text/html');
-							htmlDocs.push(htmlItem);
-						}
-
-						data = await loadEPUB({ htmlDocs, mwbYear: issue.substring(0, 4), lang: language });
-						fixture = (await import(`./fixtures/mwb_${language}_${issue}.js`)).default;
-					}
 				}
 
 				resolve({ data, fixture });
--- test/03_downloadParsing.test.js
+++ test/03_downloadParsing.test.js
@@ -62,52 +62,6 @@ describe('Live download enhanced parsing', () => {
 							mergedSources.push(obj);
 						}
 
-						if (!hasEPUB) {
-							const url =
-								JW_FINDER +
-								new URLSearchParams({
-									wtlocale: language,
-									pub: 'mwb',
-									issue: issueDate,
-								});
-
-							const res = await fetch(url);
-							const result = await res.text();
-
-							const parser = new window.DOMParser();
-							const htmlItem = parser.parseFromString(result, 'text/html');
-
-							const docIds = [];
-							const accordionItems = htmlItem.getElementsByClassName('docClass-106');
-							for (const weekLink of accordionItems) {
-								weekLink.classList.forEach((item) => {
-									if (item.indexOf('docId-') !== -1) {
-										docIds.push(item.split('-')[1]);
-									}
-								});
-							}
-
-							const htmlDocs = [];
-							for (let z = 0; z < docIds.length; z++) {
-								const docId = docIds[z];
-								const finderLink = `https://www.jw.org/finder?wtlocale=${language}&docid=${docId}`;
-								const res2 = await fetch(finderLink);
-								const result2 = await res2.text();
-
-								const parser = new window.DOMParser();
-								const htmlItem = parser.parseFromString(result2, 'text/html');
-								htmlDocs.push(htmlItem);
-							}
-
-							const htmlData = await loadEPUB({ htmlDocs, mwbYear: currentYear, lang: language });
-							const obj = {
-								issueDate,
-								...htmlData,
-							};
-
-							mergedSources.push(obj);
-						}
-
 						// assigning next issue
 						monthMwb = monthMwb + 2;
 						if (monthMwb === 13) {
