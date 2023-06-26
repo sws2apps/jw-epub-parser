7a7ca217a9afd3dd163b44304c6c4e6f1f3022d1
rhahao <26148770+rhahao@users.noreply.github.com>
2023-06-26 17:43:59 +0300

    ci(ci): add cypress E2E testing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,59 +1,44 @@
-import { fileDialog } from 'file-select-dialog';
 import { loadEPUB } from 'jw-epub-parser';
 import JSONFormatter from 'json-formatter-js';
 
 function App() {
-  const handleOpenMWBEPUB = async () => {
-    try {
-      const file = await fileDialog({
-        accept: '.epub',
-        strict: true,
-      });
+	const handleOpenMWBEPUB = async (file) => {
+		try {
+			const data = await loadEPUB(file);
+			const formatter = new JSONFormatter(data);
 
-      const data = await loadEPUB(file);
-      const formatter = new JSONFormatter(data);
+			const result = document.querySelector('#mwb_parser_result');
+			result.innerHTML = '';
+			result.appendChild(formatter.render());
+		} catch (err) {
+			console.error(err);
+		}
+	};
 
-      const result = document.querySelector('#mwb_parser_result');
-      result.innerHTML = '';
-      result.appendChild(formatter.render());
-    } catch (err) {
-      console.error(err);
-    }
-  };
+	const handleOpenWEPUB = async (file) => {
+		try {
+			const data = await loadEPUB(file);
+			const formatter = new JSONFormatter(data);
 
-  const handleOpenWEPUB = async () => {
-    try {
-      const file = await fileDialog({
-        accept: '.epub',
-        strict: true,
-      });
+			const result = document.querySelector('#w_parser_result');
+			result.innerHTML = '';
+			result.appendChild(formatter.render());
+		} catch (err) {
+			console.error(err);
+		}
+	};
 
-      const data = await loadEPUB(file);
-      const formatter = new JSONFormatter(data);
-
-      const result = document.querySelector('#w_parser_result');
-      result.innerHTML = '';
-      result.appendChild(formatter.render());
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  return (
-    <div>
-      <h1>JW EPUB Parser (in the browser)</h1>
-      <h3>Meeting Workbook Data</h3>
-      <button onClick={handleOpenMWBEPUB} type='button'>
-        Import EPUB
-      </button>
-      <div id='mwb_parser_result' style={{ marginTop: '20px' }}></div>
-      <h3>Watchtower Study Data</h3>
-      <button onClick={handleOpenWEPUB} type='button'>
-        Import EPUB
-      </button>
-      <div id='w_parser_result' style={{ marginTop: '20px' }}></div>
-    </div>
-  );
+	return (
+		<div>
+			<h1>JW EPUB Parser</h1>
+			<h3>Meeting Workbook Data</h3>
+			<input data-test='mwb_file' type='file' onChange={(e) => handleOpenMWBEPUB(e.target.files[0])}></input>
+			<div id='mwb_parser_result' style={{ marginTop: '20px' }}></div>
+			<h3>Watchtower Study Data</h3>
+			<input data-test='w_file' type='file' onChange={(e) => handleOpenWEPUB(e.target.files[0])}></input>
+			<div id='w_parser_result' style={{ marginTop: '20px' }}></div>
+		</div>
+	);
 }
 
 export default App;
