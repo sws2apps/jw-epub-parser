c648a3e8c1ebd646e9cb468a8a0b4b3c15345153
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2023-07-05 12:39:45 +0000

    feat(module): add support for madagascar sign language

--- src/browser/utils.js
+++ src/browser/utils.js
@@ -6,9 +6,10 @@ import MG from '../locales/mg-MG/text.json';
 import T from '../locales/pt-BR/text.json';
 import TND from '../locales/mg-TND/text.json';
 import TNK from '../locales/mg-TNK/text.json';
+import TTM from '../locales/mg-TTM/text.json';
 import VZ from '../locales/mg-VZ/text.json';
 
 window.path = path;
 window.jw_epub_parser = {
-  languages: { E, F, MG, T, TND, TNK, VZ },
+	languages: { E, F, MG, T, TND, TNK, TTM, VZ },
 };
--- src/node/utils.js
+++ src/node/utils.js
@@ -9,11 +9,12 @@ import MG from '../locales/mg-MG/text.json' assert { type: 'json' };
 import T from '../locales/pt-BR/text.json' assert { type: 'json' };
 import TND from '../locales/mg-TND/text.json' assert { type: 'json' };
 import TNK from '../locales/mg-TNK/text.json' assert { type: 'json' };
+import TTM from '../locales/mg-TTM/text.json' assert { type: 'json' };
 import VZ from '../locales/mg-VZ/text.json' assert { type: 'json' };
 
 window.path = path;
 window.fetch = fetch;
 window.readFile = readFile;
 window.jw_epub_parser = {
-	languages: { E, F, MG, T, TND, TNK, VZ },
+	languages: { E, F, MG, T, TND, TNK, TTM, VZ },
 };
