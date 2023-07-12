5adbf91b3b844c7207f13ac67d97909f1c15c909
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2023-07-12 13:00:31 +0000

    fix(rules): wrong week date parsed from wt study

--- example/sample.js
+++ example/sample.js
@@ -3,6 +3,22 @@ import { loadEPUB } from '../src/node/index.js';
 const JW_CDN = 'https://app.jw-cdn.org/apis/pub-media/GETPUBMEDIALINKS?';
 const JW_FINDER = 'https://www.jw.org/finder?';
 const WOL_CDN = 'https://b.jw-cdn.org/apis/wol-link/';
+const WOL_E = 'https://wol.jw.org/wol/dt/r1/lp-e';
+
+const months = [
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December',
+];
 
 const fetchIssueData = async (issue, pub) => {
 	try {
@@ -168,17 +184,182 @@ const fetchIssueData = async (issue, pub) => {
 export const fetchData = async (language, issue, pub) => {
 	let data = [];
 
-	const url = JW_CDN + new URLSearchParams({ langwritten: language, pub, output: 'json', issue });
+	if (!issue && !pub) {
+		for await (const pub of ['mwb', 'w']) {
+			const issues = [];
 
-	const res = await fetch(url);
+			if (pub === 'mwb') {
+				let notFound = false;
 
-	if (res.status === 200) {
-		const result = await res.json();
-		const hasEPUB = result.files[language].EPUB;
+				// get current issue
+				const today = new Date();
+				const day = today.getDay();
+				const diff = today.getDate() - day + (day === 0 ? -6 : 1);
+				const weekDate = new Date(today.setDate(diff));
+				const validDate = weekDate.setMonth(weekDate.getMonth());
 
-		const issueFetch = { issueDate: issue, currentYear: issue.substring(0, 4), language, hasEPUB: hasEPUB };
+				const startDate = new Date(validDate);
+				const currentMonth = startDate.getMonth() + 1;
+				const monthOdd = currentMonth % 2 === 0 ? false : true;
+				let monthMwb = monthOdd ? currentMonth : currentMonth - 1;
+				let currentYear = startDate.getFullYear();
 
-		data = await fetchIssueData(issueFetch, pub);
+				do {
+					const issueDate = currentYear + String(monthMwb).padStart(2, '0');
+					const url =
+						JW_CDN +
+						new URLSearchParams({
+							langwritten: language,
+							pub: 'mwb',
+							output: 'json',
+							issue: issueDate,
+						});
+
+					const res = await fetch(url);
+
+					if (res.status === 200) {
+						const result = await res.json();
+						const hasEPUB = result.files[language].EPUB;
+
+						issues.push({ issueDate, currentYear, language, hasEPUB: hasEPUB });
+					}
+
+					if (res.status === 404) {
+						notFound = true;
+					}
+
+					// assigning next issue
+					monthMwb = monthMwb + 2;
+					if (monthMwb === 13) {
+						monthMwb = 1;
+						currentYear++;
+					}
+				} while (notFound === false);
+			}
+
+			if (pub === 'w') {
+				let notFound = false;
+
+				// get w current issue
+				const today = new Date();
+				const url = `${WOL_E}/${today.getFullYear()}/${today.getMonth() + 1}/${today.getDate()}`;
+
+				const res = await fetch(url);
+				const data = await res.json();
+
+				const wData = data.items.find((item) => item.classification === 68);
+				const publicationTitle = wData.publicationTitle;
+
+				const findYear = /\b\d{4}\b/;
+				const array = findYear.exec(publicationTitle);
+				let currentYear = +array[0];
+
+				const monthsRegex = `(${months.join('|')})`;
+
+				const regex = new RegExp(monthsRegex);
+				const array2 = regex.exec(publicationTitle);
+
+				let monthW = months.findIndex((month) => month === array2[0]) + 1;
+
+				do {
+					const issueDate = currentYear + String(monthW).padStart(2, '0');
+					const url =
+						JW_CDN +
+						new URLSearchParams({
+							langwritten: language,
+							pub,
+							output: 'json',
+							issue: issueDate,
+						});
+
+					const res = await fetch(url);
+
+					if (res.status === 200) {
+						const result = await res.json();
+						const hasEPUB = result.files[language].EPUB;
+
+						issues.push({ issueDate, currentYear, language, hasEPUB: hasEPUB });
+					}
+
+					if (res.status === 404) {
+						notFound = true;
+					}
+
+					// assigning next issue
+					monthW = monthW + 1;
+					if (monthW === 13) {
+						monthW = 1;
+						currentYear++;
+					}
+				} while (notFound === false);
+			}
+
+			if (issues.length > 0) {
+				const fetchSource1 = fetchIssueData(issues[0], pub);
+				const fetchSource2 = issues.length > 1 ? fetchIssueData(issues[1], pub) : Promise.resolve([]);
+				const fetchSource3 = issues.length > 2 ? fetchIssueData(issues[2], pub) : Promise.resolve([]);
+				const fetchSource4 = issues.length > 3 ? fetchIssueData(issues[3], pub) : Promise.resolve([]);
+				const fetchSource5 = issues.length > 4 ? fetchIssueData(issues[4], pub) : Promise.resolve([]);
+				const fetchSource6 = issues.length > 5 ? fetchIssueData(issues[5], pub) : Promise.resolve([]);
+				const fetchSource7 = issues.length > 6 ? fetchIssueData(issues[6], pub) : Promise.resolve([]);
+
+				const allData = await Promise.all([
+					fetchSource1,
+					fetchSource2,
+					fetchSource3,
+					fetchSource4,
+					fetchSource5,
+					fetchSource6,
+					fetchSource7,
+				]);
+
+				for (let z = 0; z < allData.length; z++) {
+					const tempObj = allData[z];
+					if (tempObj.length > 0) {
+						for (const src of tempObj) {
+							const date = src.mwb_week_date || src.w_study_date;
+
+							const prevSrc = data.find((item) => item.mwb_week_date === date || item.w_study_date === date);
+
+							if (prevSrc) {
+								Object.assign(prevSrc, src);
+							}
+
+							if (!prevSrc) {
+								data.push(src);
+							}
+						}
+					}
+				}
+			}
+		}
+
+		for (const src of data) {
+			if (src.mwb_week_date) {
+				src.week_date = src.mwb_week_date;
+				delete src.mwb_week_date;
+			}
+
+			if (src.w_study_date) {
+				src.week_date = src.w_study_date;
+				delete src.w_study_date;
+			}
+		}
+	}
+
+	if (issue && pub) {
+		const url = JW_CDN + new URLSearchParams({ langwritten: language, pub, output: 'json', issue });
+
+		const res = await fetch(url);
+
+		if (res.status === 200) {
+			const result = await res.json();
+			const hasEPUB = result.files[language].EPUB;
+
+			const issueFetch = { issueDate: issue, currentYear: issue.substring(0, 4), language, hasEPUB: hasEPUB };
+
+			data = await fetchIssueData(issueFetch, pub);
+		}
 	}
 
 	return data;
--- src/common/parsing_rules.js
+++ src/common/parsing_rules.js
@@ -1,314 +1,316 @@
 import { JWEPUBParserError } from '../classes/error.js';
 import {
-  getAssignmentsName,
-  getAssignmentsVariations,
-  getCBSVariations,
-  getLivingPartsVariations,
-  getMonthNames,
-  getStudyArticleDateVariations,
-  getTGWBibleReadingVariations,
-  getTGWTalkVariations,
+	getAssignmentsName,
+	getAssignmentsVariations,
+	getCBSVariations,
+	getLivingPartsVariations,
+	getMonthNames,
+	getStudyArticleDateVariations,
+	getTGWBibleReadingVariations,
+	getTGWTalkVariations,
 } from './language_rules.js';
 
 export const extractMonthName = (src, lang) => {
-  let varDay;
-  let monthIndex;
+	let varDay;
+	let monthIndex;
 
-  src = src.split('–')[0];
+	src = src.split('–')[0];
 
-  const monthNames = getMonthNames(lang);
+	const monthNames = getMonthNames(lang);
 
-  for (const month of monthNames) {
-    const monthLang = month.name;
-    const regex = new RegExp(`(${monthLang})`);
-    const array = regex.exec(src);
+	for (const month of monthNames) {
+		const monthLang = month.name;
+		const regex = new RegExp(`(${monthLang})`);
+		const array = regex.exec(src);
 
-    if (Array.isArray(array)) {
-      varDay = +src.match(/(\d+)/)[0];
-      monthIndex = month.index;
-      break;
-    }
-  }
+		if (Array.isArray(array)) {
+			varDay = +src.match(/(\d+)/)[0];
+			monthIndex = month.index;
+			break;
+		}
+	}
 
-  if (typeof varDay === 'number' && typeof monthIndex === 'number') {
-    return { varDay, monthIndex };
-  }
+	if (typeof varDay === 'number' && typeof monthIndex === 'number') {
+		return { varDay, monthIndex };
+	}
 
-  throw new JWEPUBParserError('week-date', `Parsing failed when extracting the week date. The input was: ${src}`);
+	throw new JWEPUBParserError('week-date', `Parsing failed when extracting the week date. The input was: ${src}`);
 };
 
 export const extractSongNumber = (src) => {
-  return +src.match(/(\d+)/)[0];
+	return +src.match(/(\d+)/)[0];
 };
 
 export const extractTGWTalk = (src, lang) => {
-  const variations = getTGWTalkVariations(lang).split('|');
-  const pattern = '{{ title }}';
+	const variations = getTGWTalkVariations(lang).split('|');
+	const pattern = '{{ title }}';
 
-  let result;
-  for (const variation of variations) {
-    const startIndex = variation.indexOf(pattern);
-    const endIndex = src.length - variation.replace(pattern, '').length + startIndex;
-    const extracted = src.substring(startIndex, endIndex);
-    const verifyExtract = variation.replace(pattern, extracted);
-    if (verifyExtract === src) {
-      result = extracted;
-      break;
-    }
-  }
+	let result;
+	for (const variation of variations) {
+		const startIndex = variation.indexOf(pattern);
+		const endIndex = src.length - variation.replace(pattern, '').length + startIndex;
+		const extracted = src.substring(startIndex, endIndex);
+		const verifyExtract = variation.replace(pattern, extracted);
+		if (verifyExtract === src) {
+			result = extracted;
+			break;
+		}
+	}
 
-  if (result) return result;
+	if (result) return result;
 
-  throw new JWEPUBParserError('tgw-talk', `Parsing failed for Treasures from God’s Word part. The input was: ${src}`);
+	throw new JWEPUBParserError('tgw-talk', `Parsing failed for Treasures from God’s Word part. The input was: ${src}`);
 };
 
 export const extractTGWBibleReading = (src, lang) => {
-  const variations = getTGWBibleReadingVariations(lang).split('|');
-  const pattern = '{{ source }}';
+	const variations = getTGWBibleReadingVariations(lang).split('|');
+	const pattern = '{{ source }}';
 
-  let result;
-  for (const variation of variations) {
-    const startIndex = variation.indexOf(pattern);
-    const endIndex = src.length - variation.replace(pattern, '').length + startIndex;
-    const extracted = src.substring(startIndex, endIndex);
-    const verifyExtract = variation.replace(pattern, extracted);
-    if (verifyExtract === src) {
-      result = extracted;
-      break;
-    }
-  }
+	let result;
+	for (const variation of variations) {
+		const startIndex = variation.indexOf(pattern);
+		const endIndex = src.length - variation.replace(pattern, '').length + startIndex;
+		const extracted = src.substring(startIndex, endIndex);
+		const verifyExtract = variation.replace(pattern, extracted);
+		if (verifyExtract === src) {
+			result = extracted;
+			break;
+		}
+	}
 
-  if (result) return result;
+	if (result) return result;
 
-  throw new JWEPUBParserError('tgw-bibleReading', 'Parsing failed for Bible Reading part');
+	throw new JWEPUBParserError('tgw-bibleReading', 'Parsing failed for Bible Reading part');
 };
 
 export const extractAYFAssignment = (src, lang) => {
-  const variations = getAssignmentsVariations(lang).split('|');
-  const assignmentsName = getAssignmentsName(lang);
+	const variations = getAssignmentsVariations(lang).split('|');
+	const assignmentsName = getAssignmentsName(lang);
 
-  const patternAssignment = '{{ assignment }}';
-  const patternSource = '{{ source }}';
+	const patternAssignment = '{{ assignment }}';
+	const patternSource = '{{ source }}';
 
-  let result;
-  for (const variation of variations) {
-    const patternSourceIndex = variation.indexOf(patternSource);
+	let result;
+	for (const variation of variations) {
+		const patternSourceIndex = variation.indexOf(patternSource);
 
-    const find = variation.substring(0, patternSourceIndex).trim();
+		const find = variation.substring(0, patternSourceIndex).trim();
 
-    let assignmentsList = '(';
-    for (let a = 0; a < assignmentsName.length; a++) {
-      assignmentsList += assignmentsName[a];
+		let assignmentsList = '(';
+		for (let a = 0; a < assignmentsName.length; a++) {
+			assignmentsList += assignmentsName[a];
 
-      if (a < assignmentsName.length - 1) {
-        assignmentsList += '|';
-      }
-    }
-    assignmentsList += ')';
+			if (a < assignmentsName.length - 1) {
+				assignmentsList += '|';
+			}
+		}
+		assignmentsList += ')';
 
-    let textSearch = find.replace('{{ duration }}', '\\d+');
-    textSearch = textSearch.replace('(', '(\\(');
-    textSearch = textSearch.replace(')', ')\\)');
-    textSearch = textSearch.replace(' :', ' ?:?');
-    textSearch = textSearch.replace(') ', ') ?');
-    textSearch = textSearch.replace('??', '?');
-    textSearch = textSearch.replace(patternAssignment, assignmentsList);
+		let textSearch = find.replace('{{ duration }}', '\\d+');
+		textSearch = textSearch.replace('(', '(\\(');
+		textSearch = textSearch.replace(')', ')\\)');
+		textSearch = textSearch.replace(' :', ' ?:?');
+		textSearch = textSearch.replace(') ', ') ?');
+		textSearch = textSearch.replace('??', '?');
+		textSearch = textSearch.replace(patternAssignment, assignmentsList);
 
-    const regex = new RegExp(textSearch.trim());
-    const array = regex.exec(src);
+		const regex = new RegExp(textSearch.trim());
+		const array = regex.exec(src);
 
-    if (array !== null) {
-      const partTiming = +array[2].match(/(\d+)/)[0];
+		if (array !== null) {
+			const partTiming = +array[2].match(/(\d+)/)[0];
 
-      let textSearch = find.replace('{{ assignment }}', '');
-      textSearch = textSearch.replace('{{ duration }}', partTiming);
+			let textSearch = find.replace('{{ assignment }}', '');
+			textSearch = textSearch.replace('{{ duration }}', partTiming);
 
-      const split = src.split(textSearch.trim());
+			const split = src.split(textSearch.trim());
 
-      if (split.length === 2) {
-        const partType = split[0].trim();
+			if (split.length === 2) {
+				const partType = split[0].trim();
 
-        let textSearch = variation.replace('{{ assignment }}', partType);
-        textSearch = textSearch.replace('{{ duration }}', partTiming);
+				let textSearch = variation.replace('{{ assignment }}', partType);
+				textSearch = textSearch.replace('{{ duration }}', partTiming);
 
-        const findNextIndex = textSearch.indexOf('{{ source }}');
-        const srcNext = src.substring(findNextIndex);
+				const findNextIndex = textSearch.indexOf('{{ source }}');
+				const srcNext = src.substring(findNextIndex);
 
-        const obj = { src: srcNext, type: partType, time: partTiming };
+				const obj = { src: srcNext, type: partType, time: partTiming };
 
-        let verifyExtract = variation.replace('{{ assignment }}', obj.type);
-        verifyExtract = verifyExtract.replace('{{ duration }}', obj.time);
-        verifyExtract = verifyExtract.replace('{{ source }}', obj.src);
+				let verifyExtract = variation.replace('{{ assignment }}', obj.type);
+				verifyExtract = verifyExtract.replace('{{ duration }}', obj.time);
+				verifyExtract = verifyExtract.replace('{{ source }}', obj.src);
 
-        if (verifyExtract === src) {
-          result = obj;
-          break;
-        }
-      }
-    }
-  }
+				if (verifyExtract === src) {
+					result = obj;
+					break;
+				}
+			}
+		}
+	}
 
-  if (result) return result;
+	if (result) return result;
 
-  throw new JWEPUBParserError(
-    'ayf-part',
-    `Parsing failed for Apply Yourself to the Field Ministry part. The input was: ${src}`
-  );
+	throw new JWEPUBParserError(
+		'ayf-part',
+		`Parsing failed for Apply Yourself to the Field Ministry part. The input was: ${src}`
+	);
 };
 
 export const extractLCAssignment = (src, lang) => {
-  const variations = getLivingPartsVariations(lang).split('|');
-  const patternSource = '{{ source }}';
-  const patternDuration = '{{ duration }}';
-  const patternContent = '{{ content }}';
+	const variations = getLivingPartsVariations(lang).split('|');
+	const patternSource = '{{ source }}';
+	const patternDuration = '{{ duration }}';
+	const patternContent = '{{ content }}';
 
-  let result;
-  for (const variation of variations) {
-    const patternSourceIndex = variation.indexOf(patternSource);
-    const patternContentIndex = variation.indexOf(patternContent);
+	let result;
+	for (const variation of variations) {
+		const patternSourceIndex = variation.indexOf(patternSource);
+		const patternContentIndex = variation.indexOf(patternContent);
 
-    let masterSearch = variation.replace(patternSource, '');
-    masterSearch = masterSearch.replace(patternContent, '');
+		let masterSearch = variation.replace(patternSource, '');
+		masterSearch = masterSearch.replace(patternContent, '');
 
-    let textSearch = masterSearch.replace('{{ duration }}', '\\d+');
-    textSearch = textSearch.replace('(', '(\\(');
-    textSearch = textSearch.replace(')', ')\\)');
-    textSearch = textSearch.replace(' :', ' ?:?');
-    textSearch = textSearch.replace(') ', ') ?');
-    textSearch = textSearch.replace('??', '?');
+		let textSearch = masterSearch.replace('{{ duration }}', '\\d+');
+		textSearch = textSearch.replace('(', '(\\(');
+		textSearch = textSearch.replace(')', ')\\)');
+		textSearch = textSearch.replace(' :', ' ?:?');
+		textSearch = textSearch.replace(') ', ') ?');
+		textSearch = textSearch.replace('??', '?');
 
-    const regex = new RegExp(textSearch.trim());
-    const array = regex.exec(src);
+		const regex = new RegExp(textSearch.trim());
+		const array = regex.exec(src);
 
-    if (array !== null) {
-      const partTiming = +array[0].match(/(\d+)/)[0];
-      const findStrings = masterSearch.replace(patternDuration, partTiming);
+		if (array !== null) {
+			const partTiming = +array[0].match(/(\d+)/)[0];
+			const findStrings = masterSearch.replace(patternDuration, partTiming);
 
-      let split = src.split(findStrings);
+			let split = src.split(findStrings);
 
-      if (split.length === 1) {
-        split = src.split(findStrings.trim());
-      }
+			if (split.length === 1) {
+				split = src.split(findStrings.trim());
+			}
 
-      if (split.length === 2) {
-        let partTitle;
-        let partContent;
+			if (split.length === 2) {
+				let partTitle;
+				let partContent;
 
-        if (split[1] === '') {
-          partTitle = split[0].trim();
-          partContent = '';
-        }
+				if (split[1] === '') {
+					partTitle = split[0].trim();
+					partContent = '';
+				}
 
-        if (split[1] !== '') {
-          if (patternSourceIndex < patternContentIndex) {
-            partTitle = split[0].trim();
-            partContent = split[1].trim();
-          }
+				if (split[1] !== '') {
+					if (patternSourceIndex < patternContentIndex) {
+						partTitle = split[0].trim();
+						partContent = split[1].trim();
+					}
 
-          if (patternSourceIndex > patternContentIndex) {
-            partTitle = split[1].trim();
-            partContent = split[0].trim();
-          }
-        }
+					if (patternSourceIndex > patternContentIndex) {
+						partTitle = split[1].trim();
+						partContent = split[0].trim();
+					}
+				}
 
-        let verifyExtract = variation.replace('{{ source }}', partTitle);
-        verifyExtract = verifyExtract.replace('{{ duration }}', partTiming);
-        verifyExtract = verifyExtract.replace('{{ content }}', partContent);
+				let verifyExtract = variation.replace('{{ source }}', partTitle);
+				verifyExtract = verifyExtract.replace('{{ duration }}', partTiming);
+				verifyExtract = verifyExtract.replace('{{ content }}', partContent);
 
-        if (split[1] === '') verifyExtract = verifyExtract.trim();
+				if (split[1] === '') verifyExtract = verifyExtract.trim();
 
-        if (verifyExtract === src) {
-          result = {
-            time: partTiming,
-            title: partTitle,
-            content: partContent,
-          };
-          break;
-        }
-      }
-    }
-  }
+				if (verifyExtract === src) {
+					result = {
+						time: partTiming,
+						title: partTitle,
+						content: partContent,
+					};
+					break;
+				}
+			}
+		}
+	}
 
-  if (result) return result;
+	if (result) return result;
 
-  throw new JWEPUBParserError('lc-part', `Parsing failed for Living as Christians part. The input was: ${src}`);
+	throw new JWEPUBParserError('lc-part', `Parsing failed for Living as Christians part. The input was: ${src}`);
 };
 
 export const extractCBSSource = (src, lang) => {
-  const variations = getCBSVariations(lang).split('|');
+	const variations = getCBSVariations(lang).split('|');
 
-  const pattern = '{{ source }}';
+	const pattern = '{{ source }}';
 
-  let result;
-  for (const variation of variations) {
-    const startIndex = variation.indexOf(pattern);
-    const endIndex = src.length - variation.replace('{{ source }}', '').length + startIndex;
-    const extracted = src.substring(startIndex, endIndex);
-    const verifyExtract = variation.replace('{{ source }}', extracted);
-    if (verifyExtract === src) {
-      result = extracted;
-      break;
-    }
-  }
+	let result;
+	for (const variation of variations) {
+		const startIndex = variation.indexOf(pattern);
+		const endIndex = src.length - variation.replace('{{ source }}', '').length + startIndex;
+		const extracted = src.substring(startIndex, endIndex);
+		const verifyExtract = variation.replace('{{ source }}', extracted);
+		if (verifyExtract === src) {
+			result = extracted;
+			break;
+		}
+	}
 
-  if (result) return result;
+	if (result) return result;
 
-  throw new JWEPUBParserError('lc-cbs', `Parsing failed for Congregation Bible Study part. The input was: ${src}`);
+	throw new JWEPUBParserError('lc-cbs', `Parsing failed for Congregation Bible Study part. The input was: ${src}`);
 };
 
 export const extractLastSong = (src) => {
-  const temp = extractSongNumber(src);
-  return temp > 151 ? src : temp;
+	const temp = extractSongNumber(src);
+	return temp > 151 ? src : temp;
 };
 
 export const extractWTStudyDate = (src, lang) => {
-  const variations = getStudyArticleDateVariations(lang).split('|');
+	const variations = getStudyArticleDateVariations(lang).split('|');
 
-  const patternNumber = '{{ number }}';
-  const patternDate = '{{ date }}';
+	const patternNumber = '{{ number }}';
+	const patternDate = '{{ date }}';
 
-  let varDay;
-  let monthIndex;
-  let varYear;
+	let varDay;
+	let monthIndex;
+	let varYear;
 
-  for (const variation of variations) {
-    let textSearch = variation.replace(patternDate, '');
-    textSearch = textSearch.replace(patternNumber, '\\d+');
+	for (const variation of variations) {
+		let textSearch = variation.replace(patternDate, '');
+		textSearch = textSearch.replace(patternNumber, '\\d+');
 
-    const regex = new RegExp(textSearch.trim());
-    const array = regex.exec(src);
+		const regex = new RegExp(textSearch.trim());
+		const array = regex.exec(src);
 
-    if (array !== null) {
-      const dateStartIndex = array[0].length;
-      const dateValue = src.substring(dateStartIndex);
+		if (array !== null) {
+			const dateStartIndex = array[0].length;
+			if (dateStartIndex > 0) {
+				const dateValue = src.substring(dateStartIndex);
 
-      textSearch = dateValue.trim().split('–')[0];
+				textSearch = dateValue.trim().split('–')[0];
 
-      const monthNames = getMonthNames(lang);
+				const monthNames = getMonthNames(lang);
 
-      for (const month of monthNames) {
-        const monthLang = month.name;
-        const regex = new RegExp(`(${monthLang})`);
-        const array2 = regex.exec(textSearch);
+				for (const month of monthNames) {
+					const monthLang = month.name;
+					const regex = new RegExp(`(${monthLang})`);
+					const array2 = regex.exec(textSearch);
 
-        if (Array.isArray(array2)) {
-          varDay = +textSearch.match(/(\d+)/)[0];
-          monthIndex = month.index;
+					if (Array.isArray(array2)) {
+						varDay = +textSearch.match(/(\d+)/)[0];
+						monthIndex = month.index;
 
-          const findYear = /\b\d{4}\b/;
-          const array3 = findYear.exec(dateValue);
-          if (array3 !== null) {
-            varYear = +array3[0];
-          }
-          break;
-        }
-      }
-    }
-  }
+						const findYear = /\b\d{4}\b/;
+						const array3 = findYear.exec(dateValue);
+						if (array3 !== null) {
+							varYear = +array3[0];
+						}
+						break;
+					}
+				}
+			}
+		}
+	}
 
-  if (typeof varDay === 'number' && typeof monthIndex === 'number' && typeof varYear === 'number') {
-    return { varDay, monthIndex, varYear };
-  }
+	if (typeof varDay === 'number' && typeof monthIndex === 'number' && typeof varYear === 'number') {
+		return { varDay, monthIndex, varYear };
+	}
 
-  throw new JWEPUBParserError('wtstudy', `Parsing failed for Watchtower Study Date. The input was: ${src}`);
+	throw new JWEPUBParserError('wtstudy', `Parsing failed for Watchtower Study Date. The input was: ${src}`);
 };
