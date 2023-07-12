4229ccb47950e8f47e71b53d4872db9cb90f5a18
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2023-07-12 13:00:31 +0000

    fix(rules): wrong week date parsed from wt study

--- example/index.js
+++ example/index.js
@@ -1,31 +1,34 @@
 import { fetchData } from './sample.js';
 
 const runLiveCommand = async () => {
-  const issueIndex = process.argv.indexOf('--issue');
-  if (issueIndex === -1) {
-    console.error('issue date missing from arguments');
-    return;
-  }
+	const languageIndex = process.argv.indexOf('--language');
+	if (languageIndex === -1) {
+		console.error('language missing from arguments');
+		return;
+	}
 
-  const languageIndex = process.argv.indexOf('--language');
-  if (languageIndex === -1) {
-    console.error('language missing from arguments');
-    return;
-  }
+	const issueIndex = process.argv.indexOf('--issue');
+	const pubIndex = process.argv.indexOf('--pub');
 
-  const pubIndex = process.argv.indexOf('--pub');
-  if (pubIndex === -1) {
-    console.error('pub type missing from arguments');
-    return;
-  }
+	if (issueIndex >= 0 && pubIndex === -1) {
+		console.error('issue date was provided but pub type is missing');
+		return;
+	}
 
-  const issue = process.argv[issueIndex + 1];
-  const language = process.argv[languageIndex + 1];
-  const pub = process.argv[pubIndex + 1];
+	if (pubIndex >= 0 && issueIndex === -1) {
+		console.error('pub type was provided but issue date is missing');
+		return;
+	}
 
-  const data = await fetchData(language, issue, pub);
+	const language = process.argv[languageIndex + 1];
+	const issue = issueIndex >= 0 ? process.argv[issueIndex + 1] : undefined;
+	const pub = pubIndex >= 0 ? process.argv[pubIndex + 1] : undefined;
 
-  console.log(data);
+	console.time();
+	const data = await fetchData(language, issue, pub);
+
+	console.log(data);
+	console.timeEnd();
 };
 
 runLiveCommand();
