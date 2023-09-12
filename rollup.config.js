1f27630ce51d7b8aee5e7271428f2faec81284fe
rhahao <26148770+rhahao@users.noreply.github.com>
2023-09-12 21:34:29 +0300

    merge upstream/main

--- rollup.config.js
+++ rollup.config.js
@@ -4,30 +4,39 @@ import json from '@rollup/plugin-json';
 import terser from '@rollup/plugin-terser';
 
 const config = [
-  {
-    input: 'src/browser/index.js',
-    output: [
-      {
-        sourcemap: true,
-        file: 'dist/index.js',
-        format: 'es',
-      },
-    ],
-    plugins: [nodeResolve(), commonjs(), json(), terser()],
-    external: ['dateformat', 'jszip', 'path-browserify'],
-  },
-  {
-    input: 'src/node/index.js',
-    output: [
-      {
-        sourcemap: true,
-        file: 'dist/node/index.js',
-        format: 'es',
-      },
-    ],
-    plugins: [commonjs(), json(), terser()],
-    external: ['dateformat', 'jszip', 'global-jsdom/register', 'path', 'fs', 'node-fetch', 'fs/promises'],
-  },
+	{
+		input: 'src/browser/index.js',
+		output: [
+			{
+				sourcemap: true,
+				file: 'dist/index.js',
+				format: 'es',
+			},
+		],
+		plugins: [nodeResolve(), commonjs(), json(), terser()],
+		external: ['dateformat', 'jszip', 'path-browserify'],
+	},
+	{
+		input: 'src/node/index.js',
+		output: [
+			{
+				sourcemap: true,
+				file: 'dist/node/index.js',
+				format: 'es',
+			},
+		],
+		plugins: [commonjs(), json(), terser()],
+		external: [
+			'dateformat',
+			'jszip',
+			'global-jsdom/register',
+			'node-html-parser',
+			'path',
+			'fs',
+			'node-fetch',
+			'fs/promises',
+		],
+	},
 ];
 
 export default config;
