90034bfd4b1cb5dff2f852fb8a21123b05b40cb1
rhahao <26148770+rhahao@users.noreply.github.com>
2024-09-17 16:54:26 +0000

    fix(locales): add variations months for vezo

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -3,21 +3,23 @@ import { getInputType, validateInput } from '../common/epub_validation.js';
 import { startParse } from '../common/parser.js';
 
 export const loadEPUB = async (epubInput: string | Blob | { url: string }) => {
-	try {
-		validateInput(epubInput);
+  try {
+    validateInput(epubInput);
 
-		// Step: Validate Environment
-		const { browser } = getInputType(epubInput);
-		if (!browser) {
-			throw new Error(
-				'You are using the Browser version of jw-epub-parser. Please switch to the Node version if needed.'
-			);
-		}
+    // Step: Validate Environment
+    const { browser } = getInputType(epubInput);
+    if (!browser) {
+      throw new Error(
+        'You are using the Browser version of jw-epub-parser. Please switch to the Node version if needed.'
+      );
+    }
 
-		// Step: Start Parsing
-		const data = await startParse(epubInput);
-		return data;
-	} catch (err) {
-		console.error(err);
-	}
+    // Step: Start Parsing
+    const data = await startParse(epubInput);
+    return data;
+  } catch (err) {
+    console.error(err);
+
+    throw new Error((err as Error)?.message);
+  }
 };
