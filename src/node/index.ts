6ded852bce09b939dcf0b3218bf3a726de7a526b
rhahao <26148770+rhahao@users.noreply.github.com>
2024-09-17 16:28:21 +0000

    fix(parser): date parser issue for chinese in older w issues

--- src/node/index.ts
+++ src/node/index.ts
@@ -1,43 +1,14 @@
 import './utils.node.js';
-import { parseMWBSchedule, parseWSchedule, startParse } from '../common/parser.js';
+import { startParse } from '../common/parser.js';
 import { validateInput } from '../common/epub_validation.js';
-import { HTMLParse } from '../common/html_validation.js';
 
 export const loadEPUB = async (epubInput: string | Blob | { url: string }) => {
-	try {
-		validateInput(epubInput);
+  try {
+    validateInput(epubInput);
 
-		const data = await startParse(epubInput);
-		return data;
-	} catch (err) {
-		console.error(err);
-	}
-};
-
-export const parseMWB = (htmlString: string, mwbYear: number, mwbLang: string) => {
-	try {
-		// convert string to html
-		const htmlItem = HTMLParse(htmlString);
-		const article = htmlItem.querySelector('article')!;
-
-		// Step: Start Parsing
-		const data = parseMWBSchedule(article, mwbYear, mwbLang);
-		return data;
-	} catch (err) {
-		console.error(err);
-	}
-};
-
-export const parseW = (articleString: string, contentString: string, wLang: string) => {
-	try {
-		// convert string to html
-		const article = HTMLParse(articleString);
-		const content = HTMLParse(contentString);
-
-		// Step: Start Parsing
-		const data = parseWSchedule(article, content, wLang);
-		return data;
-	} catch (err) {
-		console.error(err);
-	}
+    const data = await startParse(epubInput);
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
 };
