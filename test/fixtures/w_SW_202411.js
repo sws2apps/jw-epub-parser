b86d1dd8b25c0d2b569459fdf22d9e8582c12f65
rhahao <26148770+rhahao@users.noreply.github.com>
2024-11-09 23:15:05 +0300

    feat(locales): add slovenian language

---
+++ test/fixtures/w_SW_202411.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    w_study_date: '2025/01/06',
+    w_study_date_locale: 'Makala ya 44: Januari 6-12, 2025',
+    w_study_title: 'Jinsi ya Kukabiliana na Ukosefu wa Haki',
+    w_study_opening_song: 33,
+    w_study_concluding_song: 38,
+  },
+  {
+    w_study_date: '2025/01/13',
+    w_study_date_locale: 'Makala ya 45: Januari 13-19, 2025',
+    w_study_title: 'Jifunze Kutokana na Maneno ya Mwisho ya Wanaume Waaminifu',
+    w_study_opening_song: 138,
+    w_study_concluding_song: 129,
+  },
+  {
+    w_study_date: '2025/01/20',
+    w_study_date_locale: 'Makala ya 46: Januari 20-26, 2025',
+    w_study_title: 'Akina Ndugu—Je, Mnajitahidi Kustahili Kuwa Watumishi wa Huduma?',
+    w_study_opening_song: 49,
+    w_study_concluding_song: 17,
+  },
+  {
+    w_study_date: '2025/01/27',
+    w_study_date_locale: 'Makala ya 47: Januari 27, 2025–Februari 2, 2025',
+    w_study_title: 'Akina Ndugu—Je, Mnajitahidi Kustahili Kutumikia Mkiwa Wazee wa Kutaniko?',
+    w_study_opening_song: 103,
+    w_study_concluding_song: 101,
+  },
+];
