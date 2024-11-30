528b8296d8c65cd8a99f680b1f002d6e3ef62d90
rhahao <26148770+rhahao@users.noreply.github.com>
2024-11-30 19:52:35 +0300

    fix(override): add entry for sw

--- src/common/override.ts
+++ src/common/override.ts
@@ -8,6 +8,10 @@ const overrides: Override = {
     'Seda artiklit uuritakse vahemikus 28. oktoobrist 3.novembrini 2024.':
       'Seda artiklit uuritakse vahemikus 28. oktoobrist 3. novembrini 2024.',
   },
+  SW: {
+    '7. Video ya Machi ya Mambo Yaliyotimizwa na Tengenezo (10 min.) Onyesha VIDEO.':
+      '7. Video ya Machi ya Mambo Yaliyotimizwa na Tengenezo (Dak. 10) Onyesha VIDEO.',
+  },
 };
 
 export default overrides;
