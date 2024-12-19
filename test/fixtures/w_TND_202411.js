0f81e535616eff082ab85d0342439988df83281e
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2024-12-19 11:11:40 +0300

    test(fixtures): update tnd sample
    Signed-off-by: Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>

--- test/fixtures/w_TND_202411.js
+++ test/fixtures/w_TND_202411.js
@@ -8,21 +8,21 @@ export default [
 	},
 	{
 	  w_study_date: '2025/01/13',
-	  w_study_date_locale: 'Lahatsoratse Fianaragne 45: 13-19 Janvie  2025',
+	  w_study_date_locale: 'Lahatsoratse Fianaragne 45: 13-19 Janvie 2025',
 	  w_study_title: 'Fianaragne Boake amy ty Hafatse Fara’e Nirehafe i Lahilahy Tsy Nivalike Rey',
 	  w_study_opening_song: 138,
 	  w_study_concluding_song: 129
 	},
 	{
 	  w_study_date: '2025/01/20',
-	  w_study_date_locale: 'Lahatsoratse Fianaragne 46: 20-26 Janvie  2025',
+	  w_study_date_locale: 'Lahatsoratse Fianaragne 46: 20-26 Janvie 2025',
 	  w_study_title: 'Ry Rahalahy, Mikezake ty ho Mpagnampe amy ty Fiangonagne vao Nareo?',
 	  w_study_opening_song: 49,
 	  w_study_concluding_song: 17
 	},
 	{
 	  w_study_date: '2025/01/27',
-	  w_study_date_locale: 'Lahatsoratse Fianaragne 47: 27 Janvie  2025–2 Fevrie  2025',
+	  w_study_date_locale: 'Lahatsoratse Fianaragne 47: 27 Janvie 2025–2 Fevrie 2025',
 	  w_study_title: 'Ry Rahalahy, Mikezake ty ho Androanavi-pagnahy vao Nareo?',
 	  w_study_opening_song: 103,
 	  w_study_concluding_song: 101
