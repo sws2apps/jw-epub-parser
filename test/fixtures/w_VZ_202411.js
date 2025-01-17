719dc7b0118e6458b515dbf372030efa59a9473f
rhahao.vj <26148770+rhahao@users.noreply.github.com>
2025-01-17 06:48:28 +0000

    chore(test): update watchtower fixtures for vezo

--- test/fixtures/w_VZ_202411.js
+++ test/fixtures/w_VZ_202411.js
@@ -1,28 +1,28 @@
 export default [
 	{
 	  w_study_date: '2025/01/06',
-	  w_study_date_locale: 'Lahatsoratsy Fianara 44: 6-12 Zanviè, 2025',
+	  w_study_date_locale: 'Lahatsoratsy Fianara 44: 6-12 Zanviè 2025',
 	  w_study_title: 'Akory ty Hiatrehanao ty Tsy Rariny?',
 	  w_study_opening_song: 33,
 	  w_study_concluding_song: 38
 	},
 	{
 	  w_study_date: '2025/01/13',
-	  w_study_date_locale: 'Lahatsoratsy Fianara 45: 13-19 Zanviè, 2025',
+	  w_study_date_locale: 'Lahatsoratsy Fianara 45: 13-19 Zanviè 2025',
 	  w_study_title: 'Ty Hafatsy Farany baka amy Lahilahy Tsy Nivaliky Rey',
 	  w_study_opening_song: 138,
 	  w_study_concluding_song: 129
 	},
 	{
 	  w_study_date: '2025/01/20',
-	  w_study_date_locale: 'Lahatsoratsy Fianara 46: 20-26 Zanviè, 2025',
+	  w_study_date_locale: 'Lahatsoratsy Fianara 46: 20-26 Zanviè 2025',
 	  w_study_title: 'Nareo Rahalahy, Miezaha ho Lasa Mpikarakara Fiangona!',
 	  w_study_opening_song: 49,
 	  w_study_concluding_song: 17
 	},
 	{
 	  w_study_date: '2025/01/27',
-	  w_study_date_locale: 'Lahatsoratsy Fianara 47: 27 Zanviè, 2025–2 Fevriè, 2025',
+	  w_study_date_locale: 'Lahatsoratsy Fianara 47: 27 Zanviè 2025–2 Fevriè 2025',
 	  w_study_title: 'Nareo Rahalahy, Miezaha ho Lasa Anti-panahy!',
 	  w_study_opening_song: 103,
 	  w_study_concluding_song: 101
