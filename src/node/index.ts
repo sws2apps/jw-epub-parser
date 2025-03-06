8fdf4a03f7b32331353439e7070ec9cefd4378f7
rhahao.vj <26148770+rhahao@users.noreply.github.com>
2025-03-06 09:57:23 +0000

    feat(module): ship package with types

--- src/node/index.ts
+++ src/node/index.ts
@@ -1,13 +1,16 @@
 import './utils.node.js';
 import { startParse } from '../common/parser.js';
 import { validateInput } from '../common/epub_validation.js';
+import { MWBSchedule, WSchedule } from '../types/index.js';
+
+export type { MWBSchedule, WSchedule }
 
 export const loadEPUB = async (epubInput: string | Blob | { url: string }) => {
   try {
     validateInput(epubInput);
 
     const data = await startParse(epubInput);
-    return data;
+    return data as any;
   } catch (err) {
     console.error(err);
 
